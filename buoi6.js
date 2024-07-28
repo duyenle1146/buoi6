667d358967afafbb0ee2cb1d8f8b20c3ada43230
duyenle1146 <lethimyduyen024412@gmail.com>
2024-07-28 21:22:34 +0700

    initial commit

---
+++ buoi6.js
@@ -0,0 +1,13 @@
+let ngaytrongtuan = 3
+if (ngaytrongtuan == 7){
+    console.log('khong đi chơi')
+}
+else if(ngaytrongtuan == 5){
+    console.log('đi bơi')
+}
+else if(ngaytrongtuan == 3){
+    console.log("đi uống nước")
+}
+else{
+    console.log("đi học")
+}
